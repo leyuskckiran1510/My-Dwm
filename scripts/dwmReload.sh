196c0ccfc80d3336b9bfd6d69cf442e23225f91b
leyuskckiran1510 <kdhakal1510@gmail.com>
2023-08-08 17:33:58 +0545

    added screenshot to file/clipboard 🙃

---
+++ scripts/dwmReload.sh
@@ -0,0 +1,14 @@
+csum=""
+new_csum=$(sha1sum $(which dwm))
+while true
+do
+    if [ "$csum" != "$new_csum" ]
+    then
+        csum=$new_csum
+        dwm 2> ~/.dwm.log
+    else
+        exit 0
+    fi
+    new_csum=$(sha1sum $(which dwm))
+    sleep 0.5
+done
